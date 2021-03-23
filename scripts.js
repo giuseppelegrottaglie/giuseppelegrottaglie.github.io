41030ff911ca81d4aacd815ba56e0cb9fefabc1d
giuseppelegrottaglie <giuseppe.legrottaglie4@gmail.com>
2021-03-23 12:23:24 +0100

    nu sacc de cose, tab def

--- scripts.js
+++ scripts.js
@@ -127,19 +127,19 @@ function myFunction() {
 //   });
 
 // Sorting table
-const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+// const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+// const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+//     v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+//     )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
-// do the work...
-document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-    const table = th.closest('table');
-    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-        .forEach(tr => table.appendChild(tr) );
-})));
+// // do the work...
+// document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+//     const table = th.closest('table');
+//     Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+//         .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+//         .forEach(tr => table.appendChild(tr) );
+// })));
 
 // Rende le cards cliccabili
 jQuery(document).ready(function($) {
